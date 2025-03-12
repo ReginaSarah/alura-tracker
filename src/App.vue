2390de37766e8a27975f8e6c66e38f0f823b71e2
regina.sarah <rsaramonferrarih@gmail.com>
2025-03-12 18:51:26 -0300

    Vue3 - Alura

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="columns is-gapless is-multiline">
+      <div class="column is-one-quarter">
+        <BarraLateral />
+      </div>
+      <div class="column is-three-quarter">
+        <Formulario />
+      </div>
+    </div>
+
+  </main>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarraLateral from './components/BarraLateral.vue'
+import Formulario from './components/Formulario.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BarraLateral,
+    Formulario,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
